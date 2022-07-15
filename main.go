62252f0b12bf693617a3494a78597abbc772dee1
Liopin <contact.pinheiro.lionel@gmail.com>
2022-07-15 16:02:28 +0200

    Remove useless messages

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func home(w http.ResponseWriter, r *http.Request){
+}
 
 func main(){
-	fmt.Printf("Hi\n")
+	http.HandleFunc("/", home)
+	fmt.Println("Server is running on port: 8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
