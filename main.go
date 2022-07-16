5cd706c5a02846cabbc0bb341142c7623b295709
Liopin <contact.pinheiro.lionel@gmail.com>
2022-07-16 13:19:37 +0200

    Change handleFunc to the getOffers function

--- main.go
+++ main.go
@@ -76,7 +76,7 @@ func home(w http.ResponseWriter, r *http.Request){
 }
 
 func main(){
-	http.HandleFunc("/", home)
+	http.HandleFunc("/", getOffers)
 	fmt.Println("Server is running on port: 8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
